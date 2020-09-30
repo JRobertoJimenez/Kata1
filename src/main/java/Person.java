b259df923a8884a14ff89185b5f56959c9dbcb34
José Roberto Jiménez <Jos Roberto Jimnez@DESKTOP-JBONSU1>
2020-09-30 13:28:03 +0100

    Begining 2

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDate;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,20 @@
  * @author José Roberto Jiménez
  */
 public class Person {
+    private final String name;
+    private final LocalDate birthdate;
+
+    public Person() {
+        this.name = null;
+        this.birthdate = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
     
 }
