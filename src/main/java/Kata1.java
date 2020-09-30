926d96fa858f74659157ad2452318dd62d499b12
José Roberto Jiménez <Jos Roberto Jimnez@DESKTOP-JBONSU1>
2020-09-30 14:14:33 +0100

    Kata 1 version1

--- src/main/java/Kata1.java
+++ src/main/java/Kata1.java
@@ -1,3 +1,8 @@
+
+
+import java.time.Month;
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +15,7 @@
  */
 public class Kata1 {
     public static void main(String[] args) {
-        
+        Person person = new Person("Luis Almeida", new Date(80, 5, 20));
+        System.out.println(person.getName()+" "+person.getAge());
     }
 }
